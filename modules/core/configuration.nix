408935566f10ab27cb545c6c303ed5fd81f4e21f
Pieter Pel <pelpieter@gmail.com>
2025-07-08 20:51:06 +0200

    Added nix-darwin support

--- modules/core/configuration.nix
+++ modules/core/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 {
   pkgs,
   host,
@@ -11,37 +7,16 @@
 
 {
   imports = [
-    ./hyprland.nix
-    ./gnome.nix
-    ./steam.nix
     ./spicetify.nix
     ./home-manager.nix
     ./stylix.nix
     ./fonts.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-
-  # Login manager
-  services.displayManager.gdm.enable = true;
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
-
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  networking.hostName = host;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
@@ -61,50 +36,11 @@
     LC_TIME = "nl_NL.UTF-8";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    description = "Pieter Pel";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "input"
-    ];
-    shell = pkgs.fish;
-  };
-
   # Enable fish so it can be used as the default shell.
   programs.fish.enable = true;
 
   # Install firefox.
   programs.firefox.enable = true;
-
-  # File manager
-  programs.thunar.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -116,17 +52,6 @@
     home-manager # So we can also rebuild home-manager as flake itself
   ];
 
-  # Fore redis to work optimally
-  boot.kernel.sysctl = {
-    "vm.overcommit_memory" = 1;
-  };
-
-  # Automatic updating
-  system.autoUpgrade = {
-    enable = true;
-    dates = "weekly";
-  };
-
   # Automatic cleanup
   nix.gc = {
     automatic = true;
@@ -135,35 +60,9 @@
   };
   nix.settings.auto-optimise-store = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Onedrive
-  # See https://nixos.wiki/wiki/OneDrive for what steps to take
-  services.onedrive.enable = false; # enable onedrive in future, now there is a notifcation bug
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # Automatic updating
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+  };
 }
---
+++ modules/darwin/configuration.nix
@@ -0,0 +1,7 @@
+# Future proofing
+{
+  ...
+}:
+
+{
+}
