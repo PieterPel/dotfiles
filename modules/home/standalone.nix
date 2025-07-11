ae747a7dff777a4b0f3bebb55d4d3e371408d28d
Pieter Pel <pelpieter@gmail.com>
2025-07-11 16:55:18 +0200

    Some stylix stuff for standalone HM

--- modules/home/standalone.nix
+++ modules/home/standalone.nix
@@ -2,10 +2,16 @@
   pkgs,
   inputs,
   username,
+  lib,
   ...
 }:
 {
   # Here you can put stuff that is only needed if home-manager is used standalone
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+
+  # Stylix has both a NixOS module and a home-manager module, not having the former
+  # requires us to do some stuff double
   imports = [
     inputs.stylix.homeModules.stylix
   ];
@@ -15,6 +21,13 @@
     polarity = "dark";
   };
 
-  home.username = username;
-  home.homeDirectory = "/home/${username}";
+  programs.nixvim.extraConfigVim = lib.mkAfter ''
+    highlight Normal guibg=none ctermbg=none
+    highlight NormalNC guibg=none ctermbg=none
+    highlight EndOfBuffer guibg=none ctermbg=none
+    highlight VertSplit guibg=none ctermbg=none
+    highlight StatusLine guibg=none ctermbg=none
+    highlight LineNr guibg=none ctermbg=none
+    highlight SignColumn guibg=none ctermbg=none
+  '';
 }
