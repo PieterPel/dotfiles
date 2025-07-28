16d3e6b18748ac612d4bd55fe3f6d0b535b20e76
Pieter Pel <pelpieter@gmail.com>
2025-07-28 12:23:45 +0200

    Fixed the import path in home/default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -5,17 +5,16 @@
 }:
 
 let
-  potentialUserModulePath = ../hosts/${host}/users/${username}/default.nix;
+  potentialUserModulePath = ../../hosts/${host}/users/${username}/default.nix;
   potentialUserModule =
     if builtins.pathExists (potentialUserModulePath) then potentialUserModulePath else { };
 
-  potentialUserProfilePath = ./profiles/user/${user-profile}.nix; # TODO: change to directory?
+  potentialUserProfilePath = ../../profiles/user/${user-profile}/default.nix;
   potentialUserProfile =
     if builtins.pathExists (potentialUserProfilePath) then potentialUserProfilePath else { };
 in
 {
   imports = [
-    # Make sure to import options.nix first
     ./options.nix
     ./home.nix
     potentialUserModule
