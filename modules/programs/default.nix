26dcedd82cfde7101fa89d49df3763dc09c267c4
Pieter Pel <pelpieter@gmail.com>
2025-08-13 10:59:45 +0200

    added default.nix for programs

---
+++ modules/programs/default.nix
@@ -0,0 +1,19 @@
+{ ...
+}:
+{
+  imports = [
+    ./options.nix
+    ./direnv.nix
+    ./fish.nix
+    ./git.nix
+    ./hyprlock.nix
+    ./spicetify.nix
+    ./starship.nix
+    ./tmux.nix
+    ./vscodium.nix
+    ./waybar.nix
+    ./zed.nix
+    ./nixvim
+    ./rofi
+  ];
+}
