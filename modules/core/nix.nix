644ca5d1c99289af095bf7e1460831ae627be4eb
Pieter Pel <pelpieter@gmail.com>
2025-08-26 23:36:04 +0200

    fixed nix gc flag

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -39,7 +39,7 @@ in
     nix.gc = {
       automatic = true;
       dates = "daily";
-      options = "--delete-older than 10d";
+      options = "--delete-older-than 10d";
     };
     nix.settings.auto-optimise-store = true;
   };
